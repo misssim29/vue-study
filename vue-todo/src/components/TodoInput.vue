a33ba5ff2b37aab40437fe76da4623bc59babed8
심주연 <misssim29@naver.com>
2022-11-18 15:00:03 +0900

    ES6 적용한 버전

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -22,14 +22,14 @@
 import ModalBox from './common/ModalBox.vue';
 
 export default {
-    data: function(){
+    data(){
         return {
             newTodoItem:"",
             showModal: false
         }
     },
     methods:{
-        addTodo: function(){
+        addTodo(){
             if(this.newTodoItem !== ''){
                 // this.$emit('이벤트 이름',인자1,인자2...);
                 this.$emit('addTodoItem',this.newTodoItem);
@@ -38,7 +38,7 @@ export default {
                 this.showModal = !this.showModal;
             }
         },
-        clearInput:function(){
+        clearInput(){
             this.newTodoItem = '';
         }
     },
